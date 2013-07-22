16abcf8a2799fcd39ac54db5362e985b62606cbe
Hugo <hugo.firth@gmail.com>
2013-07-23 08:53:08 +1000

    added namespaces and a test file

--- src/random_clj/lev.clj
+++ src/random_clj/lev.clj
@@ -1,9 +1,12 @@
+(ns random-clj.lev)
+
+
 (defn lev [w1 w2 counter]
-(if (= 0 (count w1))
-(+ (count w2) counter)
-(if (= 0 (count w2))
-(+ (count w1) counter)
-(let [diff (if (= (first w1) (first w2)) 0 1)]
-(min (lev (apply str (rest w1)) (apply str (rest w2)) (+ diff counter))
-(lev w1 (apply str (rest w2)) (+ diff counter))
-(lev (apply str (rest w1)) w2 (+ diff counter)))))))
+  (if (= 0 (count w1))
+    (+ (count w2) counter)
+      (if (= 0 (count w2))
+        (+ (count w1) counter)
+          (let [diff (if (= (first w1) (first w2)) 0 1)]
+            (min (lev (apply str (rest w1)) (apply str (rest w2)) (+ diff counter))
+              (lev w1 (apply str (rest w2)) (+ diff counter))
+              (lev (apply str (rest w1)) w2 (+ diff counter)))))))
---
+++ test/random_clj/lev_test.clj
@@ -0,0 +1,1 @@
+(ns random-clj.lev-test)
