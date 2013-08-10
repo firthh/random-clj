5462a85dda6080bfb96e0372611d59470075984f
Hugo <hugo.firth@gmail.com>
2013-08-10 18:19:03 +1000

    removed old crap implementation

--- src/random_clj/lev.clj
+++ src/random_clj/lev.clj
@@ -1,26 +1,5 @@
 (ns random-clj.lev)
 
-;; (defmulti leven 
-;;   (fn [w1 w2 counter] 
-;;     (if (= 0 (count w1))
-;;       :W1
-;;       (if (= 0 (count w2))
-;;         :W2
-;; 	:None))))
-
-;; (defmethod leven :W2 [w1 w2 counter]
-;; 	   (+ counter (count w1)))
-;; (defmethod leven :W1 [w1 w2 counter]
-;; 	   (+ counter (count w2)))
-;; (defmethod leven :None [w1 w2 counter]
-;;   (let [counter' (if (= (first w1) (first w2)) 
-;;        		     counter 
-;; 		     (inc counter))]
-;;     (min (leven (apply str (rest w1)) (apply str (rest w2)) counter')
-;;          (leven w1 (apply str (rest w2)) counter')
-;;          (leven (apply str (rest w1)) w2 counter'))))
-
-
 (defn str-rem [string]
   (apply str (rest string)))
 
