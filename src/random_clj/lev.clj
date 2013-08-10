8e3002e51676b9ca53d877ef734f5b523972c222
Hugo <hugo.firth@gmail.com>
2013-08-10 18:11:17 +1000

    added more tests and fixed implementation to be correct

--- src/random_clj/lev.clj
+++ src/random_clj/lev.clj
@@ -1,37 +1,40 @@
 (ns random-clj.lev)
 
-(defmulti leven 
-  (fn [w1 w2 counter] 
-    (if (= 0 (count w1))
-      :W1
-      (if (= 0 (count w2))
-        :W2
-	:None))))
+;; (defmulti leven 
+;;   (fn [w1 w2 counter] 
+;;     (if (= 0 (count w1))
+;;       :W1
+;;       (if (= 0 (count w2))
+;;         :W2
+;; 	:None))))
 
-(defmethod leven :W2 [w1 w2 counter]
-	   (+ counter (count w1)))
-(defmethod leven :W1 [w1 w2 counter]
-	   (+ counter (count w2)))
-(defmethod leven :None [w1 w2 counter]
-  (let [counter' (if (= (first w1) (first w2)) 
-       		     counter 
-		     (inc counter))]
-    (min (leven (apply str (rest w1)) (apply str (rest w2)) counter')
-         (leven w1 (apply str (rest w2)) counter')
-         (leven (apply str (rest w1)) w2 counter'))))
+;; (defmethod leven :W2 [w1 w2 counter]
+;; 	   (+ counter (count w1)))
+;; (defmethod leven :W1 [w1 w2 counter]
+;; 	   (+ counter (count w2)))
+;; (defmethod leven :None [w1 w2 counter]
+;;   (let [counter' (if (= (first w1) (first w2)) 
+;;        		     counter 
+;; 		     (inc counter))]
+;;     (min (leven (apply str (rest w1)) (apply str (rest w2)) counter')
+;;          (leven w1 (apply str (rest w2)) counter')
+;;          (leven (apply str (rest w1)) w2 counter'))))
 
 
-
+(defn str-rem [string]
+  (apply str (rest string)))
 
 (defn lev 
   ([w1 w2] (lev w1 w2 0))
   ([w1 w2 counter]
-  (cond 
-    (= 0 (count w1)) (+ (count w2) counter)
-    (= 0 (count w2)) (+ (count w1) counter)
-    :else (let [diff (if (= (first w1) (first w2)) 0 1)]
-          (min (lev (apply str (rest w1)) (apply str (rest w2)) (+ diff counter))
-               (lev w1 (apply str (rest w2)) (+ diff counter))
-               (lev (apply str (rest w1)) w2 (+ diff counter)))
-          )))
+     (cond 
+      (= 0 (count w1)) (+ (count w2) counter)
+      (= 0 (count w2)) (+ (count w1) counter)
+      :else (if (= (first w1) (first w2))  
+              (lev (str-rem w1) (str-rem w2) counter)
+              (min (lev  (str-rem w1)  (str-rem w2)  (inc counter))
+                   (lev  w1            (str-rem w2)  (inc counter))
+                   (lev  (str-rem w1)  w2            (inc counter)))
+              )
+      ))
   )
--- test/random_clj/lev_test.clj
+++ test/random_clj/lev_test.clj
@@ -2,6 +2,24 @@
     (:use midje.sweet)
     (:require [random-clj.lev :refer :all]))
 
-(facts "levensthein distance")
-(fact "same words have a distance of zero"
-      (lev "word" "word") => 0)
+(facts "levensthein distance"
+       (fact "same words have a distance of zero"
+             (lev "word" "word") => 0
+             (lev "new1" "new1") => 0
+             )
+       (fact "words with 1 character different"
+             (fact "handles extra character at the end"
+                   (lev "word" "words") => 1)
+             (fact "handles new character in the middle"
+                   (lev "new" "neaw") => 1)
+             (fact "handles a missing character"
+                   (lev "hello" "helo") => 1)
+             (fact "handles a duplicate character" 
+                   (lev "hello" "heello") => 1)
+             )
+       (fact "words with 2 characters different"
+             (lev "qwerty" "erty") => 2
+             (lev "asdfghjkl" "asdqghjl") => 2
+             )
+       )
+        
